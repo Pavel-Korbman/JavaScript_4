f6c7249dfee1c34ce5009d6c520a7a5617b2a8ca
Pavel <pavel@b2profit.ru>
2025-01-27 00:16:29 +0300

    1 семинар

--- Sem_1/1.js
+++
@@ -1,9 +0,0 @@
-// 
-const elements = document.querySelectorAll('div');
-const els = Array.from(elements);
-
-els.forEach((e) => {
-    if (e.getAttribute('data-active')) { console.log(e.textContent); }}
-    // console.log(e.textContent);
-
-);
