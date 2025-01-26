1587614035e58d13a00b5906e151bc30eb222111
Pavel <pavel@b2profit.ru>
2025-01-27 00:08:07 +0300

    1 семинар

--- Sem_1/1.js
+++ Sem_1/1.js
@@ -1,17 +1,4 @@
-// Сборщик мусора 
-// – фоновый процесс, который управляет удалением ненужных объектов в памяти. 
-
-
-let obj = {
-    animal: 'Dog'
-};
-
-let obj2 = obj;
-obj = null;
-
-console.log(obj2);
-console.log(obj);
-
+// 
 const elements = document.querySelectorAll('div');
 const els = Array.from(elements);
 
