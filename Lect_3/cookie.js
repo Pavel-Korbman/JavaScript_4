8299886cac124421ba9f42386a5afbf0c31c6e75
Pavel <pavel@b2profit.ru>
2025-02-19 16:52:20 +0300

    3 лекция

--- Lect_3/cookie.js
+++ Lect_3/cookie.js
@@ -6,3 +6,15 @@
 // Куки имеют максимальный объём данных 4Кб для одной пары ключ-значение, 
 // поэтому много информации записать в них не получится.
 
+// Напишем функцию setCookie(name, value, days), которая устанавливает cookie с указанным именем, значением и сроком действия в днях.
+
+const setCookie = (name, value, days) => {
+    let expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + days);
+
+    let cookieValue = encodeURIComponent(value) + '; expires=' + expirationDate.toUTCString();
+    document.cookie = name + '=' + cookieValue;
+};
+
+setCookie('username', 'Pavel Korbman', 7);
+setCookie('userinfo', 'Mister', 5);
