291b5a1d49d894bacf729702a9a2a3eb32d9be81
Pavel <pavel@b2profit.ru>
2025-02-27 11:25:14 +0300

    3 ДЗ

--- DZ_3/addproduct.js
+++ DZ_3/addproduct.js
@@ -1,20 +1,11 @@
-// Страница добавления отзыва:
-
-// Поле для ввода названия продукта.
-// Текстовое поле для самого отзыва.
-// Кнопка "Добавить отзыв", которая сохраняет отзыв о продукте в LocalStorage.
-
 const goodField = document.querySelector('.goodfield');
 const feedbackField = document.querySelector('.feedbackfield');
 const setButton = document.querySelector('.set');
 
-
-// content.textContent = localStorage.getItem('content') || '';
-// const feedbacks = localStorage.getItem(el.textContent).split(',');
-
 setButton.addEventListener('click', () => {
     if (goodField.value && feedbackField.value) {
-        const feedbacks = localStorage.getItem(goodField.value.trim()).split(',') || [];
+        const feedbacks =(localStorage.getItem(goodField.value.trim()))?  
+        localStorage.getItem(goodField.value.trim()).split(',') : [];
         feedbacks.unshift(feedbackField.value.trim());
         localStorage.setItem(goodField.value.trim(), feedbacks);
         alert('Отзыв сохранён');
@@ -22,5 +13,3 @@ setButton.addEventListener('click', () => {
         feedbackField.value = '';
     } else { alert('Необходимо заполнить все поля'); }
 });
-
-const feedbacks = localStorage.getItem('f').split(',') || [];
